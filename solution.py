2633b5f86a366ec36b230a03dfbd9b24a582b627
Artyom Babayan <114479248+tema-git@users.noreply.github.com>
2023-04-05 21:54:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -10,8 +10,8 @@ def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
     n = len(x)
     s = np.sum(x**2) 
-    l = chi2.ppf(1 - alpha/2, n)
-    r = chi2.ppf(alpha/2, n)
+    l = chi2.ppf(1 - alpha/2, 2*n)
+    r = chi2.ppf(alpha/2, 2*n)
     left = np.sqrt(s/31/l)
     right = np.sqrt(s/31/r)
     return left, right
